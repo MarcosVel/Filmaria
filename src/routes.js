2bddcb7ab00ed3de11cb54754dd1fe9392ffcefc
MarcosVel <marcos.veloso@sempreceub.com>
2021-10-28 00:05:31 -0300

    header created

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
 import Home from './pages/Home';
 
 const Routes = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path="/" component={ Home } />
       </Switch>
