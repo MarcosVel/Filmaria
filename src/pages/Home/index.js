43a1b4b5e52cfd61291954b149a20c53eef5484f
MarcosVel <marcos.veloso@sempreceub.com>
2021-10-28 22:48:54 -0300

    styled movies

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import api from "../../services/api";
+import './styles.css';
+
 export default function Home() {
+  const [ filmes, setFilmes ] = useState([]);
+
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get('r-api/?api=filmes')
+      setFilmes(response.data);
+    }
+
+    loadFilmes();
+  }, []);
+
   return (
-    <div className="App">
-      Home
+    <div className="container">
+      <div className="lista-filmes">
+        { filmes.map((filme) => {
+          return (
+            <Link to='/'>
+              <article key={ filme.id } className="card">
+                <div className="div-title">
+                  <h2>{ filme.nome }</h2>
+                </div>
+                <img src={ filme.foto } alt={ filme.nome } />
+              </article>
+            </Link>
+          )
+        }) }
+      </div>
     </div>
   );
 }
