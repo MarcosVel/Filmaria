958da02f5d7216a86313a0a578a716ae1c42a8de
MarcosVel <marcos.veloso@sempreceub.com>
2021-11-05 00:50:30 -0300

    style favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+import { FiEye, FiTrash2 } from "react-icons/fi";
+import './styles.css';
 
 export default function Favoritos() {
+  const [ filmes, setFilmes ] = useState([]);
+
+  useEffect(() => {
+    const minhaLista = localStorage.getItem('filmes');
+    setFilmes(JSON.parse(minhaLista) || []);
+
+  }, []);
+
   return (
-    <div>
-      Favoritos
+    <div className="filmes-favoritos">
+      <h1>Meus Favoritos</h1>
+
+      <ul>
+        { filmes.map((item) => (
+          <li key={ item.id }>
+            <div>
+              <img src={ item.foto } alt={ item.nome } />
+              <h3>{ item.nome }</h3>
+            </div>
+            <div>
+              <Link to={ `/filme/${ item.id }` }><FiEye size={ 25 } color='#2146db' /></Link>
+              <Link><FiTrash2 size={ 25 } color='#ed1a1a' /></Link>
+            </div>
+          </li>
+        )) }
+      </ul>
     </div>
   )
 }
